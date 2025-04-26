581d571d4056aa80a40f00afb211b723cbec7d1d
kingarmin <arminghajari1384@gmail.com>
2025-04-26 21:28:39 +0330

    gui start_frame has been complited

--- design.py
+++ design.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage,StringVar,Label,Frame
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage,StringVar,Label,Frame,N
 import os
 
 
@@ -30,6 +30,11 @@ canvas = Canvas(
 )
 canvas.place(x = 0 , y = 0)
 canvas.pack()
+canvas.create_rectangle(0, 0, resizer(1000,'x'), resizer(85,'y'), fill="#38b6ff", outline="")
+logo=PhotoImage(master=frame_start , file=paths('image.png'))
+label = Label(frame_start, image=logo , bg='#38b6ff')
+label.image = logo
+label.place(x=resizer(200,'x'),y=0)
 students_button_image=PhotoImage(master=frame_start,file=paths('button.png'))
 students_button = Button(
     frame_start,
@@ -46,5 +51,40 @@ students_button.place(
     width=resizer(245,'x'),
     height=resizer(245,'y')
 )
+
+teachers_button_image=PhotoImage(master=frame_start,file=paths('button_1.png'))
+teachers_button = Button(
+    frame_start,
+    image=teachers_button_image,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda:print('teachers'),
+    relief="flat",
+    bg='white'
+)
+teachers_button.place(
+    x=resizer(362,'x'),
+    y=resizer(421,'y'),
+    width=resizer(245,'x'),
+    height=resizer(245,'y')
+)
+
+managers_button_image=PhotoImage(master=frame_start,file=paths('button_2.png'))
+managers_button = Button(
+    frame_start,
+    image=managers_button_image,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda:print('managers'),
+    relief="flat",
+    bg='white'
+)
+managers_button.place(
+    x=resizer(362,'x'),
+    y=resizer(727,'y'),
+    width=resizer(245,'x'),
+    height=resizer(245,'y')
+)
+
 window.resizable(False, False)
 window.mainloop()
